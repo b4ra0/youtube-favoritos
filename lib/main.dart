3289e15ab37939fc619972c767f05a8a90ad7728
Lucas Barão <lucas.baraom@gmail.com>
2022-05-03 08:45:37 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:favoritos_youtube/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Youtube Favoritos',
+      home: HomeScreen(),
+    );
+  }
+}
